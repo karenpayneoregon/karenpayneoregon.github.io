aff1b817d1b9f7ebc31978fbf01c2efc79b26995
Karen Payne <Karen.1.Payne@oregon.gov>
2021-06-23 16:12:26 -0700

    Minor code refactor

--- csharp/Aricles/NewToYou1/Account.cs
+++ csharp/Aricles/NewToYou1/Account.cs
@@ -1,4 +1,4 @@
-﻿namespace WindowsFormsApp2
+﻿namespace CodeSample
 {
     public class Account 
     {
--- csharp/Aricles/NewToYou1/AccountOperations.cs
+++ csharp/Aricles/NewToYou1/AccountOperations.cs
@@ -4,7 +4,7 @@ using System.IO;
 using System.Linq;
 using System.Text;
 
-namespace WindowsFormsApp2
+namespace CodeSample
 {
     public class AccountOperations
     {
